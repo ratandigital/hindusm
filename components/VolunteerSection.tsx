b77c71b4ae76310ac9f28712fa1e44af851a60d4
Ratan Kumar Baidya <90468456+ratandigital@users.noreply.github.com>
2025-03-06 10:58:58 +0600

    Update VolunteerSection.tsx

--- components/VolunteerSection.tsx
+++ components/VolunteerSection.tsx
@@ -4,44 +4,10 @@ import Slider from "react-slick";
 import React from 'react'
 import VolunteerSlider from "./VolunteerSlider";
 import VolunterrSocialTegger from "./VolunterrSocialTegger";
-// Custom Previous Arrow
-// const PrevArrow = (props: any) => {
-//     const { onClick } = props;
-//     return (
-//       <button className="slick-prev custom-arrow" onClick={onClick}>
-//         <i className="far fa-arrow-left"></i>
-//       </button>
-//     );
-//   };
-  
-//   // Custom Next Arrow
-//   const NextArrow = (props: any) => {
-//     const { onClick } = props;
-//     return (
-//       <button className="slick-next custom-arrow" onClick={onClick}>
-//         <i className="far fa-arrow-right"></i>
-//       </button>
-//     );
-//   };
+
 
 export default function VolunteerSection() {
-    // const settings = {
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     arrows: true,
-    //     dots: false,
-    //     autoplay: false,
-    //     prevArrow: <PrevArrow />,
-    //     nextArrow: <NextArrow />,
-    //     responsive: [
-    //       {
-    //         breakpoint: 991,
-    //         settings: {
-    //           arrows: false,
-    //         },
-    //       },
-    //     ],
-    //   };
+  
   return (
     <section className="section section-padding light-bg">
     <div className="container">
